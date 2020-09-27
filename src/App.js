3a55ab353ed03b6c14a70eff08c170edf0fc3547
Wills Blake <richwblake@gmail.com>
2020-09-26 23:13:37 -0400

    added create todo container

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import TodoList from './Components/todoList';
+import CreateTodo from './Components/createTodo';
+import EditTodo from './Components/editTodo';
 
 export default class App extends Component {
   render() {
     return(
       <Router>
         <div className='container'>
-          <h2>MERN Stack Todo App</h2>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href='wills.fyi' target="_blank">
+              <img src='https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/320/htc/37/whale_1f40b.png' width="30" height="30" alt="CodingTheSmartWay.com" />
+            </a>
+            <Link to='/' className='navbar-brand'>MERN Todo App</Link>
+            <div className='collapse navbar-collapse'>
+              <ul className='navbar-nav mr-auto'>
+                <li className='navbar-item'>
+                  <Link to='/' className='nav-link'>Todos</Link>
+                </li>
+                <li className='navbar-item'>
+                  <Link to='/createtodo' className='nav-link'>Create Todo</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <Route exact path='/' component={TodoList} />
+          <Route exact path='/createtodo' component={CreateTodo} />
+          <Route exact path='edittodo/:id' component={EditTodo} /> 
         </div>
       </Router>
     )
--- src/Components/createTodo.js
+++ src/Components/createTodo.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
 
+const initialState = {
+  todoDescription: '',
+  todoResponsible: '',
+  todoPriority: '',
+  todoCompleted: false
+}
+
 export default class CreateTodo extends Component {
+
+  constructor() {
+    super();
+    this.state = initialState;
+  }
+
+  handleOnChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log('Form Submitted!');
+    console.log(`Description: ${this.state.todoDescription}`);
+    console.log(`Person(s) Responsible: ${this.state.todoResponsible}`);
+    console.log(`Priority: ${this.state.todoPriority}`);
+
+    this.setState(initialState);
+  }
+
+
   render() {
     return(
-      <div>
-        <h2>Create Todo Component</h2>
+      <div style={{ marginTop: 10 }}>
+        <h3>Create new Todo</h3>
       </div>
     )
   }
